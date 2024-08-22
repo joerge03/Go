8c35cb1b31a7d837511b291c019de526e871d816
joerge03 <joergemichaels@gmail.com>
2024-08-22 18:09:57 +0800

    update front-end authentication service

--- front-end/authentication-service/cmd/api/main.go
+++ front-end/authentication-service/cmd/api/main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
 
 	"authentication/data"
 )
 
 const port = "8080"
+
+var counts int64
 
 type Config struct {
 	DB     *sql.DB
@@ -14,4 +21,62 @@ type Config struct {
 }
 
 func main() {
+	db := connectToDb()
+
+	if db == nil {
+		log.Fatal("Cannot connect right now hiii hiii.")
+	}
+
+	app := Config{
+		DB:     db,
+		Models: data.New(db),
+	}
+
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: app.routes(),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Something wrong with the server, err:", err)
+	}
+
+	fmt.Printf("Running on localhosts:%v\n", port)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("db", dsn)
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func connectToDb() *sql.DB {
+	dsnToken := os.Getenv("DSN")
+
+	for {
+		db, err := openDB(dsnToken)
+		if err != nil {
+			fmt.Printf("there is a err %v", err)
+			counts++
+		} else {
+			return db
+		}
+
+		if counts > 10 {
+			fmt.Printf("It took to many attempts of reconnecting")
+			return nil
+		}
+
+		fmt.Println("sleeping hehe XD HIIII HIIII. OWWW")
+		time.Sleep(2 * time.Second)
+		continue
+	}
 }

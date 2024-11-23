d2ab3c7aaf4f167bd026dc650523451a6f816075
joerge03 <joergemichaels@gmail.com>
2024-11-23 15:31:53 +0800

    update tune 2

---
+++ tune2/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+
+func main(){
+	fmt.Printf("%v", strings.Repeat("-",30))
+}
--- tune2/ssh/sshHarvester.go
+++ tune2/ssh/sshHarvester.go
@@ -134,6 +134,7 @@ func (servers *SSHServers) String() string{
 	*serverString, err = ToJSON(*servers, true)
 	if err != nil{		
 		for _, server := range *servers {
+			fmt.Println("updateing servers")
 			*serverString += fmt.Sprintf("%+v\n%s\n", server, strings.Repeat("-",30))
 		}
 	}
@@ -167,7 +168,6 @@ func IsUserAuthority(s *SSHServer) IsUserAuthorityCallback{
 	return func(p ssh.PublicKey) bool {
 		return true
 	}
-
 }
 
 // type HostKeyCallbackType func(hostName string, remote net.Addr, key ssh.PublicKey) error 

